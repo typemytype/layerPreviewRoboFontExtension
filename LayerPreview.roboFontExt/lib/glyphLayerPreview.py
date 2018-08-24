f71308a4850138b0a341b351fb71e7cb32c5234f
Frederik Berlaen <frederik@typemytype.com>
2018-08-24 23:29:27 +0200

    py3 ufo3 RF3

--- LayerPreview.roboFontExt/lib/glyphLayerPreview.py
+++ LayerPreview.roboFontExt/lib/glyphLayerPreview.py
@@ -1,7 +1,18 @@
-from AppKit import *
-from mojo.glyphPreview import GlyphPreview, GlyphPreviewView
+import AppKit
 
-class GlyphLayerPreviewView(GlyphPreviewView):
+from mojo.glyphPreview import GlyphPreview
+
+from mojo.roboFont import version
+
+inRF3 = version >= "3.0"
+
+if inRF3:
+    from mojo.glyphPreview import RFGlyphPreviewView
+else:
+    from mojo.glyphPreview import GlyphPreviewView as RFGlyphPreviewView
+
+
+class GlyphLayerPreviewView(RFGlyphPreviewView):
 
     def init(self):
         super(GlyphLayerPreviewView, self).init()
@@ -19,46 +30,66 @@ class GlyphLayerPreviewView(GlyphPreviewView):
         if self._glyph is None:
             return
 
-        transform = NSAffineTransform.transform()
+        transform = AppKit.NSAffineTransform.transform()
         transform.translateXBy_yBy_(0, self._buffer)
         transform.concat()
 
-        transform = NSAffineTransform.transform()
+        transform = AppKit.NSAffineTransform.transform()
         transform.scaleBy_(self._scale)
         transform.translateXBy_yBy_(0, self._descender)
         transform.concat()
 
-        flipTransform = NSAffineTransform.transform()
+        flipTransform = AppKit.NSAffineTransform.transform()
         flipTransform.translateXBy_yBy_(self._shift, self._upm)
         flipTransform.scaleXBy_yBy_(1.0, -1.0)
         flipTransform.concat()
 
-
         glyph = self._glyph
-        if glyph.isLayer():
-            glyph = glyph.getBaseGlyph()
+        if not inRF3:
+            if glyph.isLayer():
+                glyph = glyph.getBaseGlyph()
 
-        font = glyph.getParent()
+        if inRF3:
+            layer = glyph.layer
+        else:
+            layer = glyph.getParent()
 
         if self._color is not None:
             self._color.set()
 
-        for layerName in reversed(["foreground"] + font.layerOrder):
-            layer = glyph.getLayer(layerName)
+        if inRF3:
+            layerNames = layer.layerSet.layerOrder
+        else:
+            layerNames = ["foreground"] + layer.layerOrder
+
+        for layerName in reversed(layerNames):
+            if inRF3:
+                layerGlyph = glyph.getLayerGlyph(layerName)
+                layerColor = layerGlyph.layer.color
+                if layerColor:
+                    color = AppKit.NSColor.colorWithCalibratedRed_green_blue_alpha_(layerColor.r, layerColor.g, layerColor.b, layerColor.a)
+                else:
+                    color = AppKit.NSColor.blackColor()
+            else:
+                layerGlyph = glyph.getLayer(layerName)
+                color = layer.getLayerColor(layerName)
+
             if self._color is None:
-                color = font.getLayerColor(layerName)
                 color.set()
-            path = layer.getRepresentation("defconAppKit.NSBezierPath")
+
+            path = layerGlyph.getRepresentation("defconAppKit.NSBezierPath")
+
             path.fill()
 
         if self._selection:
-            selectionPath = NSBezierPath.bezierPath()
-            radius = 3/self._scale
+            selectionPath = AppKit.NSBezierPath.bezierPath()
+            radius = 3 / self._scale
             for x, y in self._selection:
-                selectionPath.appendBezierPathWithOvalInRect_(NSMakeRect(x-radius, y-radius, radius*2, radius*2))
+                selectionPath.appendBezierPathWithOvalInRect_(AppKit.NSMakeRect(x - radius, y - radius, radius * 2, radius * 2))
 
-            NSColor.redColor().set()
+            AppKit.NSColor.redColor().set()
             selectionPath.fill()
+
 
 class GlyphLayerPreview(GlyphPreview):
 
--- LayerPreview.roboFontExt/lib/layerWindow.py
+++ LayerPreview.roboFontExt/lib/layerWindow.py
@@ -8,33 +8,35 @@ from mojo.events import addObserver, removeObserver
 
 from mojo.roboFont import *
 
+
 class LayerWindow(BaseWindowController):
-    
+
     def __init__(self):
-                
-        self.w = Window((400, 400), "Layer Preview", minSize=(400, 300))
-        
+
+        self.w = Window((420, 400), "Layer Preview", minSize=(420, 300))
+
         self.w.preview = GlyphLayerPreview((0, 0, -0, -30))
         self.currentGlyphChanged()
-        
+
         self.w.useColor = CheckBox((10, -30, 100, 22), "Use Color:", callback=self.useColorCallback)
-        
-        self.w.color = ColorWell((120, -35, 40, 30), color=NSColor.blackColor(), callback=self.colorCallback)
-        
-        self.w.testInstall = Button((-170, -30, -10, 22), "Test Install Layers", callback=self.testInstallCallback)
-        
+
+        self.w.color = ColorWell((100, -35, 40, 30), color=NSColor.blackColor(), callback=self.colorCallback)
+
+        self.w.splitLayers = Button((-260, -30, -160, 22), "Split Layers", callback=self.splitLayerCallback)
+        self.w.testInstall = Button((-150, -30, -10, 22), "Test Install Layers", callback=self.testInstallCallback)
+
         addObserver(self, "currentGlyphChanged", "currentGlyphChanged")
-        
+
         self.setUpBaseWindowBehavior()
         self.w.open()
-        
+
     def currentGlyphChanged(self, notification=None):
         self.w.preview.setGlyph(CurrentGlyph())
-    
+
     def windowCloseCallback(self, sender):
         removeObserver(self, "currentGlyphChanged")
         super(LayerWindow, self).windowCloseCallback(sender)
-    
+
     def useColorCallback(self, sender):
         value = sender.get()
         self.w.color.enable(value)
@@ -43,32 +45,46 @@ class LayerWindow(BaseWindowController):
         else:
             color = None
         self.w.preview.setColor(color)
-    
+
     def colorCallback(self, sender):
         self.useColorCallback(self.w.useColor)
-    
-    
-    def testInstallCallback(self, sender):
-        font = CurrentFont()
-        
+
+    def _getLayerFonts(self, font):
         familyName = font.info.familyName
         styleName = font.info.styleName
         if familyName is None or styleName is None:
             self.showMessage("Font needs a family name and style name.", "Please name it correctly")
-            return  
-        
-        font.testInstall()
-        
+            return []
+
+        layerFonts = []
         for layerName in font.layerOrder:
-            layerFont = NewFont(font.info.familyName, "%s %s" %(font.info.styleName, layerName), showUI=True)
-            
+            layerFont = NewFont(showInterface=False)
+            layerFont.info.update(font.info)
+            layerFont.info.styleName = "%s %s" % (font.info.styleName, layerName)
+
+            layerFont.kerning.update(font.kerning)
+            layerFont.groups.update(font.groups)
+
             for glyph in font:
                 layerGlyph = glyph.getLayer(layerName)
                 layerFont[glyph.name] = layerGlyph.copy()
-                                                
-            layerFont.testInstall()
-                
-            
-        
-    
+            layerFonts.append(layerFont)
+
+        return layerFonts
+
+    def splitLayerCallback(self, sender):
+        font = CurrentFont()
+        layerFonts = self._getLayerFonts(font)
+        for layerFont in layerFonts:
+            layerFont.openInterface()
+
+    def testInstallCallback(self, sender):
+        font = CurrentFont()
+        layerFonts = self._getLayerFonts(font)
+        if layerFonts:
+            font.testInstall()
+            for layerFont in layerFonts:
+                layerFont.testInstall()
+
+
 LayerWindow()
